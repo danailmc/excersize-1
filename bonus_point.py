f2e814f342e20ed685215ea089266d87f0223757
danailmc <61781871+danailmc@users.noreply.github.com>
2020-03-29 16:38:13 +0300

    create  bonus point

---
+++ bonus_point.py
@@ -0,0 +1,17 @@
+number = int (input ())
+bonus = 0
+
+if number <= 100:
+    bonus = 5
+elif number > 100
+    bonus = number * 0.2
+elif number > 1000
+    bonus number * 0.1
+if number % 2 == 0
+    bonus = bonus + 1
+elif number % 5 == 0
+    if number % 2 != 0
+        bonus = bonus + 2
+
+print (bonus)
+print (number + bonus)
