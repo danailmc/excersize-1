04dab91cc22c9951e35af0826f2f49a217e6229a
danailmc <61781871+danailmc@users.noreply.github.com>
2020-03-29 17:12:09 +0300

    Create convert measures

---
+++ convert_messures.py
@@ -0,0 +1,21 @@
+distance = float (input ())
+input_measure = input ()
+output_measure = input ()
+
+if input_measure == "m":
+    if output_measure == "mm"
+        result = distance * 1000
+    elif output_measure == "cm"
+        result = distance * 100
+         if input_measure == "cm"
+    elif output_measure == "mm"
+        result = distance * 10
+         elif output_measure == "m"
+        result = distance * 0.01
+    elif input_measure == "mm"
+       if output_measure == "cm":
+           result = distance * 0.1
+        elif output_maesure == "m":
+            result = distance * 0.001
+
+print (f"{result: .3f}")
