f18a8b7afa2dc3495555aa65f0c74803674fc2f9
danailmc <61781871+danailmc@users.noreply.github.com>
2020-03-29 16:45:14 +0300

    create speed information

---
+++ speed_information.py
@@ -0,0 +1,11 @@
+speed = float(input())
+if sped <= 10
+    print ("slow")
+elif speed <= 50
+    print ("avarage")
+elif speed <= 150
+    print ("fast")
+elif speed <= 1000
+    print ("ultra fast")
+else:
+    print ("extremely fast")
