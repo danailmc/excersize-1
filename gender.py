38c7e6c9a79cbe2215ac25d86729981613457a52
danailmc <61781871+danailmc@users.noreply.github.com>
2020-03-30 14:59:10 +0300

    create gender

---
+++ gender.py
@@ -0,0 +1,12 @@
+age = float (input())
+gender = input ()
+
+if gender == "f" and age < 16:
+    print ("Miss")
+
+elif gender == "f" age >= 16:
+    print ("Ms.")
+elif gender == "m" and age >= 16:
+    print ("Master")
+elif gender == "m" and age >=16:
+    print ("Mr")
