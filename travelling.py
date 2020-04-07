e78c7af2e23167d6ecd88289a71a82a64317b24b
danailmc <61781871+danailmc@users.noreply.github.com>
2020-04-07 17:10:29 +0300

    Create travelling

---
+++ travelling.py
@@ -0,0 +1,16 @@
+town = input()
+
+while town != "End":
+    minimum_budget = float(input())
+    current_money_saved = 0
+
+    whille minimum_budget > current_money_saved:
+    money = float (input())
+    current_money_saved += money
+
+    if current_money_saved >= minimum_budget:
+        print(f"Going to {town}")
+
+    town = input ()
+
+    
